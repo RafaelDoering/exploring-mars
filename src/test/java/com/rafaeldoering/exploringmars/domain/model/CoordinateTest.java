e9d2e9fb969ee15f28fe5b68a1390d15a6ee028b
Rafael Doering <rafael.doering.soares@usp.br>
2022-01-19 00:47:52 -0300

    feat: create coordinate model

---
+++ src/test/java/com/rafaeldoering/exploringmars/domain/model/CoordinateTest.java
@@ -0,0 +1,33 @@
+package com.rafaeldoering.exploringmars.domain.model;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import org.junit.jupiter.api.Test;
+
+public class CoordinateTest {
+  @Test
+  public void givenValidDataWhenInstantiateCoordinateThenReturnCoordinate() throws Exception {
+    Coordinate coordinates = new Coordinate(10, 10);
+
+    assertSame(10, coordinates.getX());
+    assertSame(10, coordinates.getY());
+  }
+
+  @Test
+  public void givenInvalidXWhenInstantiateCoordinateThenThrownError() throws Exception {    
+    try {
+      new Coordinate(-10, 10);
+    } catch (Exception error){
+      assertSame("X coordinate can only be a positive value", error.getMessage());
+    }
+  }
+
+  @Test
+  public void givenInvalidYWhenInstantiateCoordinateThenThrownError() throws Exception {    
+    try {
+      new Coordinate(10, -10);
+    } catch (Exception error){
+      assertSame("Y coordinate can only be a positive value", error.getMessage());
+    }
+  }
+}
